65e539986f58e09e381093e25212ec2d260593cb
tiandic <145258834+tiandic@users.noreply.github.com>
2025-08-29 14:57:28 +0800

    Fixed the issue of app.res when compiling with gcc.

--- make.bat
+++ make.bat
@@ -2,8 +2,10 @@
 
 mkdir out
 
+windres app.rc -o out/app_res.o
+
 gcc -static -shared .\copyHint.c .\config.c hookdll.c -o out/hookdll.dll -luser32 -lgdi32 -lyaml
-gcc -static -mwindows .\main.c app.res -o out/copyOptimize.exe
+gcc -static -mwindows .\main.c out/app_res.o -o out/copyOptimize.exe
 gcc -static -mwindows .\recoverCursor.c -o out/recoverCursor.exe
 
 pip install pyinstaller
@@ -16,4 +18,5 @@ pyinstaller -F get_explorer_dir.py --distpath out
 copy config.yaml out
 
 rmdir /s /q build
+del out\app_res.o
 del *.spec
